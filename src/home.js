975f12d8ffb836dd404f96095f59be22bf87a77c
AbdulRhman Hassn <bodahassan12@gmail.com>
2025-09-05 23:11:56 +0300

    Add the Home Page using a separate module and make the header buttons start to work

---
+++ src/home.js
@@ -0,0 +1,34 @@
+import img from "./main-img.jpg";
+
+export default function createHomePage() {
+  const mainPage = document.createElement("div");
+  const mainText = document.createElement("article");
+  const mainTitle = document.createElement("h1");
+  const separator = document.createElement("div");
+  const mainParagraph = document.createElement("p");
+  const mainBtn = document.createElement("button");
+  const mainImg = document.createElement("img");
+
+  mainTitle.textContent = "SERVING YOU COMFORT FOOD MADE WITH LOVE SINCE 2025";
+  mainParagraph.textContent = ` From sizzling grills to freshly baked bread, every dish is crafted with care
+                                to bring warmth to your table. Whether you’re stopping by for a quick
+                                bite or sharing a meal with friends and family, we believe good food
+                                should feel like home. Our promise is simple: fresh ingredients, generous
+                                portions, and flavors that make you smile every time.`;
+  mainBtn.textContent = "ORDER NOW";
+  mainImg.src = img;
+  mainImg.alt = "Food";
+
+  mainPage.classList.add("main-page");
+  mainText.classList.add("main-text");
+  mainTitle.classList.add("main-title");
+  separator.classList.add("separator");
+  mainParagraph.classList.add("main-paragraph");
+  mainBtn.classList.add("main-btn");
+  mainImg.classList.add("main-img");
+
+  mainText.append(mainTitle, separator, mainParagraph, mainBtn);
+  mainPage.append(mainText, mainImg);
+
+  return mainPage;
+};
--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 import "./styles.css";
+import createHomePage from "./home";
 
-console.log("Hello World");
+const headerBtns = document.querySelectorAll(".header-btn");
+const home = document.querySelector(".home");
+const menu = document.querySelector(".menu");
+const contact = document.querySelector(".contact");
+const about = document.querySelector(".about");
+const content = document.getElementById("content");
+
+home.addEventListener("click", () => {
+  content.appendChild(createHomePage());
+});
+
+menu.addEventListener("click", () => {
+  content.removeChild(content.firstChild);
+});
+
+contact.addEventListener("click", () => {
+  content.removeChild(content.firstChild);
+});
+
+about.addEventListener("click", () => {
+  content.removeChild(content.firstChild);
+});
+
+
+// Make the border fixed to current selected button
+headerBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    headerBtns.forEach((btn) => btn.classList.remove("active"));
+    btn.classList.add("active");
+  });
+});
